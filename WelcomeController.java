b7fbc069a660c5932039a3d6e9dc22ce98bd374a
rimjhimsudhesh <110277428+rimjhimsudhesh@users.noreply.github.com>
2023-07-19 00:02:36 -0700

    Create WelcomeController.java

---
+++ WelcomeController.java
@@ -0,0 +1,13 @@
+package com.javaguides.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to spring boot world";
+    }
+}
