5be7fbbaf6cafe038a16c578f33763c4fff01cb6
rimjhimsudhesh <110277428+rimjhimsudhesh@users.noreply.github.com>
2023-08-15 11:42:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,81 @@
-const express = require('express');
-const app = express();
-const tops = ['Black - top',
-                     'Blue - top',
-                     'Red - top',
-                     'Navy - top',
-                     'Gray - top',
-                     'Brown - top',
-                     'Green - top',
-                     'Yellow - top',
-                     'Purple - top',
-                     'Orange - top'];
+const tops = [
+  // ... URLs for tops images
+  'https://i.imgur.com/gvqurx8.jpg',
+  'https://i.imgur.com/WqHlX07.jpg',
+  'https://i.imgur.com/k3Pxv0p.jpg',
+  'https://i.imgur.com/xGUCIXh.jpg',
+  'https://i.imgur.com/KuoDENE.jpg',
+  'https://i.imgur.com/OAqIQLm.jpg',
+  'https://i.imgur.com/dced5KK.jpg',
+  'https://i.imgur.com/uc5K5cd.jpg',
+  'https://i.imgur.com/TsDhK7i.jpg',
+  'https://i.imgur.com/v2BRBFC.jpg',
+  'https://i.imgur.com/vBZg7iS.jpg'
 
-const bottoms = ['Black - bottom',
-                        'White - bottom',
-                        'Purple - bottom',
-                        'Beige - bottom',
-                        'Pink - bottom',
-                        'Cream - bottom',
-                        'Brown - bottom',
-                        'Gray - bottom',
-                        'Silver - bottom',
-                        'Blue - bottom'];
+];
 
-app.use(express.static('public'));
+const bottoms = [
+  // ... URLs for bottoms images
+  'https://i.imgur.com/oOIdDHx.jpg',
+  'https://i.imgur.com/vaTo5LL.jpg',
+  'https://i.imgur.com/pHBYx2v.jpg',
+  'https://i.imgur.com/CBIIMU7.jpg',
+  'https://i.imgur.com/9IVoICz.jpg',
+  'https://i.imgur.com/salKTIQ.jpg',
+  'https://i.imgur.com/7VAnZw6.jpg',
+  'https://i.imgur.com/JUVaGsg.jpg',
+  'https://i.imgur.com/3LaQZxy.jpg',
+  'https://i.imgur.com/FmBfWOj.jpg',
+  'https://i.imgur.com/yKYzgKT.jpg'
+];
 
-app.get('/random-greeting', (req, res) => {
-    const randomGreeting = tops[Math.floor(Math.random() * tops.length)];
-    res.send(randomGreeting);
+// Tops image variables
+let currentImageIndexTops = 0; // Initialize the index for Tops images
+const imageElementTops = document.getElementById('imageTops'); // Get the image element for Tops images
+const prevButtonTops = document.getElementById('prevButtonTops'); // Get the "Previous" button for Tops images
+const nextButtonTops = document.getElementById('nextButtonTops'); // Get the "Next" button for Tops images
+
+// Bottoms image variables
+let currentImageIndexBottoms = 0; // Initialize the index for Bottoms images
+const imageElementBottoms = document.getElementById('imageBottoms'); // Get the image element for Bottoms images
+const prevButtonBottoms = document.getElementById('prevButtonBottoms'); // Get the "Previous" button for Bottoms images
+const nextButtonBottoms = document.getElementById('nextButtonBottoms'); // Get the "Next" button for Bottoms images
+
+// Rest of your code, including showImageTops, showImageBottoms, and event listeners...
+
+
+// Show tops image
+function showImageTops(index) {
+  imageElementTops.src = tops[index];
+}
+
+// Show bottoms image
+function showImageBottoms(index) {
+  imageElementBottoms.src = bottoms[index];
+}
+
+// Add event listeners for tops and bottoms navigation buttons
+prevButtonTops.addEventListener('click', () => {
+  currentImageIndexTops = Math.max(0, currentImageIndexTops - 1);
+  showImageTops(currentImageIndexTops);
 });
 
-app.get('/random-bottom', (req, res) => {
-    const randomGreeting = bottoms[Math.floor(Math.random() * bottoms.length)];
-    res.send(randomGreeting);
+nextButtonTops.addEventListener('click', () => {
+  currentImageIndexTops = Math.min(tops.length - 1, currentImageIndexTops + 1);
+  showImageTops(currentImageIndexTops);
 });
 
-const port = 3000; // You can choose any available port number
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+prevButtonBottoms.addEventListener('click', () => {
+  currentImageIndexBottoms = Math.max(0, currentImageIndexBottoms - 1);
+  showImageBottoms(currentImageIndexBottoms);
 });
+
+nextButtonBottoms.addEventListener('click', () => {
+  currentImageIndexBottoms = Math.min(bottoms.length - 1, currentImageIndexBottoms + 1);
+  showImageBottoms(currentImageIndexBottoms);
+});
+
+// Initial display
+showImageTops(currentImageIndexTops);
+showImageBottoms(currentImageIndexBottoms);
+
